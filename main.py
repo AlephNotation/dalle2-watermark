9106918d4f1c6b43d8175a71410127fcd7623e1c
Tynan Daly <tynan.s.daly@gmail.com>
2022-06-03 17:50:23 -0500

    rename watermark function

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def draw_all_boxes(img_arr, color_list: list[tuple[int, int, int]]):
     return img_arr
 
 @app.post("/add_watermark")
-async def add_watermark_to_image(resize: bool=False, file: UploadFile = File(...)):    
+async def add_watermark_to_image(resize: bool=False, image: UploadFile = File(...)):    
     contents = await file.read()
     temp = BytesIO(contents)
     img = Image.open(temp).convert("RGB")
